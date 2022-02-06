ea79ca48ad080057fc5ce5cfdd5d05934984cd31
Mahdi Bahari <baharimahdi93@gmail.com>
2022-02-06 10:04:42 +0330

    sweet

--- myadmin/admin.py
+++ myadmin/admin.py
@@ -4,9 +4,9 @@ from django.utils.translation import gettext_lazy
 
 class MyAdminSite(admin.AdminSite):
     enable_nav_sidebar = False
-    site_title = gettext_lazy('Pec')
+    site_title = gettext_lazy('Sweet')
 
-    site_header = gettext_lazy('Pec administration')
+    site_header = gettext_lazy('Sweet Bakery administration')
 
-    index_title = gettext_lazy('Site administration')
+    index_title = gettext_lazy('Sweet Bakery administration')
 
