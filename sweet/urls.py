1e12fc72d403ceb3d3fc6f8e501ad7a32b1c3220
Mahdi Bahari <baharimahdi93@gmail.com>
2022-02-11 12:49:55 +0330

    .

--- sweet/urls.py
+++ sweet/urls.py
@@ -46,7 +46,6 @@ urlpatterns = [
 
                   path('admin/', admin.site.urls),
                   path('api/v1/', include('sweet.urlsv1'))
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += []
