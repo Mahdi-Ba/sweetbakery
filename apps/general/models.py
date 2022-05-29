aa8446e8f8ce09a5d346a91d17c84457a878f5f2
baharimahdi93 <baharimahdi93@gmail.com>
2022-05-29 20:00:26 +0430

    shipping cost

--- apps/general/models.py
+++ apps/general/models.py
@@ -37,5 +37,4 @@ class Scheduling(models.Model):
     is_enable = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'time is {self.deliver_date_time} - location address in {self.location.address}' \
-               f' ,this province {self.location.province}'
+        return f'time is {self.deliver_date_time} - location address in {self.location.address}' 
