c91b8d406941f02178f76ae0d35c2928ee87e16b
baharimahdi93 <baharimahdi93@gmail.com>
2022-05-30 09:28:14 +0430

    index

--- apps/market/admin.py
+++ apps/market/admin.py
@@ -249,6 +249,7 @@ class OrderAdmin(ExportActionMixin, admin.ModelAdmin):
                    'scheduling__location__province',
                    ['scheduling__deliver_date_time', DateTimeRangeFilter],
                    ]
+    readonly_fields = ('scheduling')
 
     def mobile(self, instance):
         return instance.user.mobile
